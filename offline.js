e93e3bc249bcad70bd106f5d83e9a4563ccd2080
liedcee <67959768+liedcee@users.noreply.github.com>
2021-05-19 13:26:11 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1621441492,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/point-sheet0.png",
+		"images/point-sheet1.png",
+		"images/line-sheet0.png",
+		"images/background-sheet0.png",
+		"images/animal-sheet0.png",
+		"images/animal-sheet1.png",
+		"images/animal-sheet2.png",
+		"images/next-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/laranjado.ogg",
+		"media/verde.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
